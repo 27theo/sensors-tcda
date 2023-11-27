85a4f3404ae8174fcfe7843737b9fdee21e89666
27theo <theocaddis@gmail.com>
2023-11-27 21:03:17 +0000

    fix: change back to 20 sensors

--- update_sensors/__init__.py
+++ update_sensors/__init__.py
@@ -2,7 +2,7 @@ import azure.functions as func
 
 import common.sensor
 
-NUMBER_OF_SENSORS = 10
+NUMBER_OF_SENSORS = 20
 
 def main(mytimer: func.TimerRequest, sensors: func.Out[func.SqlRowList]) -> None:
     generated = common.sensor.generate(NUMBER_OF_SENSORS)
